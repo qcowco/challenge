5cf49fdc16cfe11abaacadebbe82b228cbe0e01a
Kamil Szuberski <kamil.szuberski@kontomatik.com>
2021-01-12 13:36:42 +0100

    Add an AuthRequest mapper method

--- src/main/java/pl/kontomatik/challenge/mapper/IpkoMapper.java
+++ src/main/java/pl/kontomatik/challenge/mapper/IpkoMapper.java
@@ -3,9 +3,15 @@ package pl.kontomatik.challenge.mapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.kontomatik.challenge.navigator.dto.AuthRequest;
 import pl.kontomatik.challenge.navigator.dto.AuthResponse;
 
 public class IpkoMapper {
+    private int version = 3;
+    private String location = "";
+    private String authStateId = "state_id";
+    private String action = "submit";
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public AuthResponse getAuthResponseFrom(String responseBody) throws JsonProcessingException {
@@ -16,5 +22,20 @@ public class IpkoMapper {
         boolean hasErrors = responseNode.with("fields").has("errors");
 
         return new AuthResponse(flowId, token, hasErrors);
+    }
+
+    public String getAuthRequestBodyFor(String fingerprint,
+                                        String username, int sequenceNumber) throws JsonProcessingException {
+        AuthRequest authRequest = AuthRequest.builder()
+                .setVersion(version)
+                .setSeq(sequenceNumber)
+                .setLocation(location)
+                .setStateId(authStateId)
+                .putData("login", username)
+                .putData("fingerprint", fingerprint)
+                .setAction(action)
+                .build();
+
+        return objectMapper.writeValueAsString(authRequest);
     }
 }
--- src/test/java/pl/kontomatik/challenge/mapper/IpkoMapperTest.java
+++ src/test/java/pl/kontomatik/challenge/mapper/IpkoMapperTest.java
@@ -1,7 +1,9 @@
 package pl.kontomatik.challenge.mapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import pl.kontomatik.challenge.navigator.dto.AuthRequest;
 import pl.kontomatik.challenge.navigator.dto.AuthResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,6 +12,7 @@ class IpkoMapperTest {
     private static final String LOGIN_RESPONSE_BODY = "{\"flow_id\":\"flow_id\",\"token\":\"token\",\"finished\":true}";
 
     private IpkoMapper ipkoMapper = new IpkoMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void givenMapsAuthResponse_thenReturnsAuthResponse() throws JsonProcessingException {
@@ -25,5 +28,35 @@ class IpkoMapperTest {
 
         // then
         assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void givenMapsStartAuthRequest_thenReturnsAuthRequestJson() throws JsonProcessingException {
+        // given
+        int version = 3;
+        int sequenceNumber = 0;
+        String location = "";
+        String stateId = "state_id";
+        String fingerprint = "fingerprint";
+        String username = "username";
+        String action = "submit";
+
+        AuthRequest expectedRequest = AuthRequest.builder()
+                .setVersion(version)
+                .setSeq(sequenceNumber)
+                .setLocation(location)
+                .setStateId(stateId)
+                .putData("login", username)
+                .putData("fingerprint", fingerprint)
+                .setAction(action)
+                .build();
+
+        String expectedJsonBody = objectMapper.writeValueAsString(expectedRequest);
+
+        // when
+        String actualJsonBody = ipkoMapper.getAuthRequestBodyFor(fingerprint, username, sequenceNumber);
+
+        // then
+        assertEquals(expectedJsonBody, actualJsonBody);
     }
 }
