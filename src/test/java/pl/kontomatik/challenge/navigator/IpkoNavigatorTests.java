ac584d8d9a5d7b7189e2b03f1b4cebb7bca0b5d6
Kamil Szuberski <kamil.szuberski@kontomatik.com>
2021-01-14 13:40:22 +0100

    Replace messy JSoup mocks with a mockup http server

--- src/test/java/pl/kontomatik/challenge/navigator/IpkoNavigatorTests.java
+++ src/test/java/pl/kontomatik/challenge/navigator/IpkoNavigatorTests.java
@@ -1,33 +1,30 @@
 package pl.kontomatik.challenge.navigator;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.junit.jupiter.MockServerExtension;
 import pl.kontomatik.challenge.exception.InvalidCredentialsException;
 import pl.kontomatik.challenge.exception.NotAuthenticatedException;
 import pl.kontomatik.challenge.mapper.IpkoMapper;
 import pl.kontomatik.challenge.mapper.IpkoMapperImpl;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.startsWith;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, MockServerExtension.class})
 public class IpkoNavigatorTests {
-    private static final String LOGIN_URL = "https://www.ipko.pl/ipko3/login";
-    private static final String NDCD_URL = "https://www.ipko.pl/nudatasecurity/2.2/w/w-573441/init/js";
-    private static final String INIT_URL = "https://www.ipko.pl/ipko3/init";
+    private static final String MOCK_URL_TEMPLATE = "http://localhost:%d%s";
+    private static final String LOGIN_PATH = "/ipko3/login";
+    private static final String NDCD_PATH = "/nudatasecurity/2.2/w/w-573441/init/js";
+    private static final String INIT_PATH = "/ipko3/init";
 
     private static final String USERNAME = "USERNAME";
     private static final String PASSWORD = "PASSWORD";
@@ -42,62 +39,40 @@ public class IpkoNavigatorTests {
     private static final String ACCOUNT_NUMBER = "123456789";
     private static final double ACCOUNT_BALANCE = 0.5;
 
-    private static final Map<String, String> COOKIES = new HashMap<>();
-
     private static IpkoNavigator bankNavigator;
     private static IpkoMapper ipkoMapper = new IpkoMapperImpl();
 
-    @Mock(answer = Answers.RETURNS_SELF)
-    private Connection loginConnection;
-
-    @Mock(answer = Answers.RETURNS_SELF)
-    private Connection cookieConnection;
-
-    @Mock(answer = Answers.RETURNS_SELF)
-    private Connection accountConnection;
-
-    @Mock
-    private Connection.Response loginResponse;
-
-    @Mock
-    private Connection.Response cookieResponse;
-
-    @Mock
-    private Connection.Response accountResponse;
-
-
     @BeforeEach
-    public void setupEach() {
+    public void setupEach(MockServerClient client, ClientAndServer clientAndServer) {
+        setupBankNavigator(clientAndServer);
+
+        client.clear(request());
+    }
+
+    private void setupBankNavigator(ClientAndServer clientAndServer) {
         bankNavigator = new IpkoNavigator(ipkoMapper);
+
+        setupMockUrls(clientAndServer.getPort());
+    }
+
+    private void setupMockUrls(Integer port) {
+        bankNavigator.setLoginUrl(getMockUrl(port, LOGIN_PATH));
+        bankNavigator.setNdcdUrl(getMockUrl(port, NDCD_PATH));
+        bankNavigator.setInitUrl(getMockUrl(port, INIT_PATH));
+    }
+
+    private static String getMockUrl(Integer port, String path) {
+        return String.format(MOCK_URL_TEMPLATE, port, path);
     }
 
     @Test
-    public void givenAuthenticationCheck_whenLoggedIn_thenReturnsIsAuthenticated() throws IOException {
+    public void givenAuthenticationCheck_whenLoggedIn_thenReturnsIsAuthenticated(MockServerClient mockServerClient) throws IOException {
         // given
-        try (MockedStatic<Jsoup> jsoup = mockStatic(Jsoup.class)) {
-            jsoup.when(() -> Jsoup.connect(LOGIN_URL))
-                    .thenReturn(loginConnection);
+        mockSuccessfulLogin(mockServerClient);
 
-            given(loginConnection.execute())
-                    .willReturn(loginResponse);
+        mockCookieRequest(mockServerClient);
 
-            given(loginResponse.headers())
-                    .willReturn(Map.of(SESSION_HEADER, SESSION_TOKEN));
-
-            given(loginResponse.body())
-                    .willReturn(LOGIN_RESPONSE_BODY);
-
-            jsoup.when(() -> Jsoup.connect(startsWith(NDCD_URL)))
-                    .thenReturn(cookieConnection);
-
-            given(cookieConnection.execute())
-                    .willReturn(cookieResponse);
-
-            given(cookieResponse.cookies())
-                    .willReturn(COOKIES);
-
-            bankNavigator.login(USERNAME, PASSWORD);
-        }
+        bankNavigator.login(USERNAME, PASSWORD);
 
         // when
         boolean authenticated = bankNavigator.isAuthenticated();
@@ -106,33 +81,29 @@ public class IpkoNavigatorTests {
         assertTrue(authenticated);
     }
 
+    private void mockSuccessfulLogin(MockServerClient mockServerClient) {
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/ipko3/login"))
+                .respond(response()
+                        .withStatusCode(200)
+                        .withHeader(SESSION_HEADER, SESSION_TOKEN)
+                        .withBody(LOGIN_RESPONSE_BODY)
+                );
+    }
+
     @Test
-    public void givenAuthenticationCheck_whenNotLoggedIn_thenReturnsNotAuthenticated() throws IOException {
+    public void givenAuthenticationCheck_whenNotLoggedIn_thenReturnsNotAuthenticated(MockServerClient mockServerClient) throws IOException {
         // given
-        try (MockedStatic<Jsoup> jsoup = mockStatic(Jsoup.class)) {
-            jsoup.when(() -> Jsoup.connect(LOGIN_URL))
-                    .thenReturn(loginConnection);
+        mockFailedLogin(mockServerClient);
 
-            given(loginConnection.execute())
-                    .willReturn(loginResponse);
+        mockCookieRequest(mockServerClient);
 
-            given(loginResponse.body())
-                    .willReturn(BAD_LOGIN_RESPONSE_BODY);
+        try {
+            bankNavigator.login(USERNAME, PASSWORD);
+        } catch (InvalidCredentialsException exception) {
 
-            jsoup.when(() -> Jsoup.connect(startsWith(NDCD_URL)))
-                    .thenReturn(cookieConnection);
-
-            given(cookieConnection.execute())
-                    .willReturn(cookieResponse);
-
-            given(cookieResponse.cookies())
-                    .willReturn(COOKIES);
-
-            try {
-                bankNavigator.login(USERNAME, PASSWORD);
-            } catch (InvalidCredentialsException exception) {
-
-            }
         }
 
         // when
@@ -142,75 +113,65 @@ public class IpkoNavigatorTests {
         assertFalse(authenticated);
     }
 
-    @Test
-    public void givenLoggingIn_whenFails_thenThrows_LoginFailedException() throws IOException {
-        try (MockedStatic<Jsoup> jsoup = mockStatic(Jsoup.class)) {
-            jsoup.when(() -> Jsoup.connect(LOGIN_URL))
-                    .thenReturn(loginConnection);
-
-            given(loginConnection.execute())
-                    .willReturn(loginResponse);
-
-            given(loginResponse.body())
-                    .willReturn(BAD_LOGIN_RESPONSE_BODY);
-
-            jsoup.when(() -> Jsoup.connect(startsWith(NDCD_URL)))
-                    .thenReturn(cookieConnection);
-
-            given(cookieConnection.execute())
-                    .willReturn(cookieResponse);
-
-            given(cookieResponse.cookies())
-                    .willReturn(COOKIES);
-
-            assertThrows(InvalidCredentialsException.class, () -> bankNavigator.login(USERNAME, PASSWORD));
-        }
+    private void mockFailedLogin(MockServerClient mockServerClient) {
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/ipko3/login"))
+                .respond(response()
+                        .withStatusCode(200)
+                        .withBody(BAD_LOGIN_RESPONSE_BODY)
+                );
     }
 
     @Test
-    public void givenRequestingAccounts_whenLoggedIn_thenReturnsAccounts() throws IOException {
+    public void givenLoggingIn_whenFails_thenThrows_LoginFailedException(MockServerClient mockServerClient) {
         // given
-        try (MockedStatic<Jsoup> jsoup = mockStatic(Jsoup.class)) {
-            jsoup.when(() -> Jsoup.connect(LOGIN_URL))
-                    .thenReturn(loginConnection);
+        mockFailedLogin(mockServerClient);
 
-            given(loginConnection.execute())
-                    .willReturn(loginResponse);
+        mockCookieRequest(mockServerClient);
 
-            given(loginResponse.headers())
-                    .willReturn(Map.of(SESSION_HEADER, SESSION_TOKEN));
+        // when/then
+        assertThrows(InvalidCredentialsException.class, () -> bankNavigator.login(USERNAME, PASSWORD));
+    }
 
-            given(loginResponse.body())
-                    .willReturn(LOGIN_RESPONSE_BODY);
+    private void mockCookieRequest(MockServerClient mockServerClient) {
+        mockServerClient
+                .when(request()
+                        .withMethod("GET")
+                        .withPath(NDCD_PATH))
+                .respond(response()
+                        .withStatusCode(200));
+    }
 
-            jsoup.when(() -> Jsoup.connect(startsWith(NDCD_URL)))
-                    .thenReturn(cookieConnection);
+    @Test
+    public void givenRequestingAccounts_whenLoggedIn_thenReturnsAccounts(MockServerClient mockServerClient) throws IOException {
+        // given
+        mockSuccessfulLogin(mockServerClient);
 
-            given(cookieConnection.execute())
-                    .willReturn(cookieResponse);
+        mockCookieRequest(mockServerClient);
 
-            given(cookieResponse.cookies())
-                    .willReturn(COOKIES);
+        mockAccountsRequest(mockServerClient);
 
-            jsoup.when(() -> Jsoup.connect(INIT_URL))
-                    .thenReturn(accountConnection);
+        Map<String, Double> expectedAccounts = Map.of(ACCOUNT_NUMBER, ACCOUNT_BALANCE);
 
-            given(accountConnection.execute())
-                    .willReturn(accountResponse);
+        bankNavigator.login(USERNAME, PASSWORD);
 
-            given(accountResponse.body())
-                    .willReturn(ACCOUNT_RESPONSE_BODY);
+        // when
+        Map<String, Double> accounts = bankNavigator.getAccounts();
 
-            Map<String, Double> expectedAccounts = Map.of(ACCOUNT_NUMBER, ACCOUNT_BALANCE);
+        // then
+        assertEquals(expectedAccounts, accounts);
+    }
 
-            bankNavigator.login(USERNAME, PASSWORD);
-
-            // when
-            Map<String, Double> accounts = bankNavigator.getAccounts();
-
-            // then
-            assertEquals(expectedAccounts, accounts);
-        }
+    private void mockAccountsRequest(MockServerClient mockServerClient) {
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath(INIT_PATH))
+                .respond(response()
+                        .withStatusCode(200)
+                        .withBody(ACCOUNT_RESPONSE_BODY));
     }
 
     @Test
