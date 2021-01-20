1c1db0e484fe31bc4343a92e23199e84a7b97556
Kamil Szuberski <kamil.szuberski@kontomatik.com>
2021-01-20 10:58:16 +0100

    Add CLI to integration test

--- src/integration-test/java/pl/kontomatik/challenge/navigator/BankNavigatorCLITest.java
+++ src/integration-test/java/pl/kontomatik/challenge/navigator/BankNavigatorCLITest.java
@@ -1,6 +1,7 @@
 package pl.kontomatik.challenge.navigator;
 
 import org.junit.jupiter.api.Test;
+import pl.kontomatik.challenge.commandline.BankNavigatorCLI;
 import pl.kontomatik.challenge.mapper.IpkoMapper;
 import pl.kontomatik.challenge.mapper.IpkoMapperImpl;
 
@@ -8,44 +9,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class IpkoNavigatorTest {
+public class BankNavigatorCLITest {
     private final String RESOURCE_NAME = "application.properties";
-
-    @Test
-    public void continuesOnCorrectCredentials() throws IOException {
-        // given
-        IpkoMapper ipkoMapper = new IpkoMapperImpl();
-        BankNavigator bankNavigator = new IpkoNavigator(ipkoMapper);
-        Properties testData = getTestData();
-
-        String username = testData.getProperty("username");
-        String password = testData.getProperty("password");
-
-        // when/then
-        assertDoesNotThrow(() -> bankNavigator.login(username, password));
-    }
 
     @Test
     public void afterSignInCanFetchAccounts() throws IOException {
         // given
         IpkoMapper ipkoMapper = new IpkoMapperImpl();
         BankNavigator bankNavigator = new IpkoNavigator(ipkoMapper);
-        Properties testData = getTestData();
-
-        String username = testData.getProperty("username");
-        String password = testData.getProperty("password");
-
-        bankNavigator.login(username, password);
+        Properties credentials = credentialsFromProperties();
+        Iterator<String> input = inputFrom(credentials);
+        List<String> output = new LinkedList<>();
+        BankNavigatorCLI bankNavigatorCLI = new BankNavigatorCLI(bankNavigator, input::next, output::add);
 
         // when/then
-        assertDoesNotThrow(bankNavigator::getAccounts);
+        assertDoesNotThrow(bankNavigatorCLI::run);
     }
 
-    private Properties getTestData() throws IOException {
+    private Iterator<String> inputFrom(Properties credentials) {
+        String username = credentials.getProperty("username");
+        String password = credentials.getProperty("password");
+
+        return List.of(username, password).iterator();
+    }
+
+    private Properties credentialsFromProperties() throws IOException {
         Properties properties = new Properties();
         InputStream inputStream = Files.newInputStream(getTestResourcePath());
         properties.load(inputStream);
