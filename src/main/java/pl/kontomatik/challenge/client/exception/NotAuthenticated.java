f8da306f45a9f14fa510ceacfc791b4334b1e9b5
Kamil Szuberski <kamil.szuberski@kontomatik.com>
2021-01-25 13:31:39 +0100

    Remove unnecessary NotAuthenticated exception

--- src/main/java/pl/kontomatik/challenge/client/exception/NotAuthenticated.java
+++
@@ -1,7 +0,0 @@
-package pl.kontomatik.challenge.client.exception;
-
-public class NotAuthenticated extends RuntimeException {
-  public NotAuthenticated(String message) {
-    super(message);
-  }
-}
